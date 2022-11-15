93614030b81a053f4032d3afaff4ef9d92bc1c73
luan-murilo-de-oliveira-e-souza <luan.oliveiraesouza@gmail.com>
2022-11-15 17:43:58 +0000

    answers exercises

--- exercises/exercise-2/exercise-2.js
+++ exercises/exercise-2/exercise-2.js
@@ -70,3 +70,32 @@ let hogwarts = [
     occupation: "Teacher",
   },
 ];
+
+//task 1
+function showGryffindor(house) {
+  const filterGryffindor = house.filter(
+    (person) => person.house === "Gryffindor"
+  );
+
+  const [person1, person2, person3, person4, person5] = filterGryffindor;
+
+  console.log(`${person1.firstName} ${person1.lastName}`);
+  console.log(`${person2.firstName} ${person2.lastName}`);
+  console.log(`${person3.firstName} ${person3.lastName}`);
+  console.log(`${person4.firstName} ${person4.lastName}`);
+  console.log(`${person5.firstName} ${person5.lastName}`);
+}
+
+showGryffindor(hogwarts);
+
+//task 2
+function teacherHasPet(hogwarts) {
+  const filterTeacherHasPet = hogwarts.filter(
+    (person) => person.occupation === "Teacher" && person.pet
+  );
+
+  const [person1] = filterTeacherHasPet;
+  console.log(`${person1.firstName} ${person1.lastName}`);
+}
+
+teacherHasPet(hogwarts);
--- exercises/exercise-3/exercise-3.js
+++ exercises/exercise-3/exercise-3.js
@@ -6,3 +6,21 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPrice: 1.0 },
   { itemName: "Hash Brown", quantity: 4, unitPrice: 0.4 },
 ];
+
+let sumTotal = "";
+let total = "";
+console.log(`QTY\t ITEM\t\t\t TOTAL`);
+for (const { quantity, itemName, unitPrice } of order) {
+  if (itemName.length < 14) {
+    console.log(
+      `${quantity}\t ${itemName}\t\t ${(quantity * unitPrice).toFixed(2)}`
+    );
+  } else {
+    console.log(
+      `${quantity}\t ${itemName}\t ${(quantity * unitPrice).toFixed(2)}`
+    );
+  }
+  total = Number(total) + Number(quantity) * Number(unitPrice);
+}
+
+console.log(`\nTotal: ${total}`);
